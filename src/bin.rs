95d9a66264bfe4ecdd7251cab6586a27ba660193
NyxCode <moritz.bischof1@gmail.com>
2019-09-04 15:23:45 +0200

    update README.md

--- src/bin.rs
+++ src/bin.rs
@@ -2,9 +2,9 @@ use ublocks_ascii_art::*;
 
 fn main() -> Result<(), SomeErr> {
     let text = std::env::args().skip(1).next().unwrap_or_else(|| "Hey!".to_owned());
-    let rendered = render_text::<RotatedRaster90<SimpleRaster>>(&text,
+    let rendered = render_text::<SimpleRaster>(&text,
                                                Font::BadScript,
-                                               (90.0, 200.0))?;
+                                               (200.0, 100.0))?;
     println!("{}", rendered);
 
     Ok(())
